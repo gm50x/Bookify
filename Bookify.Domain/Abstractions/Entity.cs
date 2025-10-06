b52aed4d305749fed0b72931f738b416d3d49369
gm50x <getuliomagela@outlook.com>
2025-10-06 20:54:21 -0300

    feat: adds domain events abstractions

--- Bookify.Domain/Abstractions/Entity.cs
+++ Bookify.Domain/Abstractions/Entity.cs
@@ -1,5 +1,13 @@
 ﻿namespace Bookify.Domain.Abstractions;
 public abstract class Entity(Guid id)
 {
+    private readonly List<IDomainEvent> _domainEvents = [];
     public Guid Id { get; init; } = id;
+
+    public IReadOnlyList<IDomainEvent> GetDomainEvents => _domainEvents.AsReadOnly();
+
+    protected void RaiseDomainEvent(IDomainEvent @event)
+    {
+        _domainEvents.Add(@event);
+    }
 }
