8847ea352d8146ee608239651d2dfcad4f956980
gm50x <getuliomagela@outlook.com>
2025-10-21 20:49:09 -0300

    feat: added logging and validation behaviors

---
+++ src/Bookify.Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,42 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+using Bookify.Application.Exceptions;
+using FluentValidation;
+using MediatR;
+
+namespace Bookify.Application.Behaviors;
+internal class ValidationBehavior<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IBaseCommand
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (!_validators.Any())
+        {
+            return await next(cancellationToken);
+        }
+
+        var context = new ValidationContext<TRequest>(request);
+
+        var validationErrors = _validators
+            .Select(validator => validator.Validate(request))
+            .Where(result => result.Errors.Any())
+            .SelectMany(result => result.Errors)
+            .Select(error => new ValidationError(error.PropertyName, error.ErrorMessage))
+            .ToList();
+
+        if (validationErrors.Any())
+        {
+            throw new Exceptions.ValidationException(validationErrors);
+        }
+
+        return await next(cancellationToken);
+
+    }
+}
--- src/Bookify.Application/DependencyInjection.cs
+++ src/Bookify.Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Bookify.Domain.Bookings;
+﻿using Bookify.Application.Behaviors;
+using Bookify.Domain.Bookings;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Bookify.Application;
@@ -10,7 +12,11 @@ public static class DependencyInjection
         services.AddMediatR(config =>
         {
             config.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
         });
+
+        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
 
         services.AddTransient<PricingService>();
 
